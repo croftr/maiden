c1a2112f552c495037b584aaa0d327c55519ad63
robin croft <rob@robincroft.com>
2024-03-16 15:11:47 +0000

    style changes

--- src/App.jsx
+++ src/App.jsx
@@ -8,41 +8,39 @@ import m2 from './assets/mobile/i2.jpg'
 import './App.css'
 
 function App() {
-  
+
   const [type, setType] = useState('desktop');
 
-  const [images, setImages] = useState([d1,d2]);
+  const [images, setImages] = useState([d1, d2]);
 
   const onChangeType = () => {
     if (type === 'desktop') {
-      setImages([m1,m2]);
+      setImages([m1, m2]);
     } else {
-      setImages([d1,d2]);
+      setImages([d1, d2]);
     }
     setType(type === 'desktop' ? 'mobile' : 'desktop');
   }
 
   return (
-    <div className='wrapper' style={{ display: "flex", flexDirection: "column", flex: 1, width: "100%" }}>
-    
-    <div className="header" style={{ width: "100%" }}>
-     <h1 >Header</h1>
-     </div> 
-    
-<div>
-  <p>hello mike</p>
-</div>
 
-    <button onClick={onChangeType}>{type}</button>
+    <>
+      <div className='header'>
+        <h1 >Header</h1>
+      </div>
 
-    <div className="thumbnails" style={{ display: "flex", gap: 8, minHeight: "calc(100vh - 356px)" }}>
-     {images.map((image, index) => (<div key={`image-${index}`}><img style={{ width: 100, height: 100 }} src={image} alt={`image-${index}`} /></div>))}
-    </div>
-    
-    <div className="footer">
-     <h1>foooter</h1>     
-    </div>
-   </div>
+      <button onClick={onChangeType}>{type}</button>
+
+      <div className="thumbnails" style={{ display: "flex", gap: 8, minHeight: "calc(100vh - 356px)", padding: 12 }}>
+        {images.map((image, index) => (<div key={`image-${index}`}><img style={{ width: 100, height: 100 }} src={image} alt={`image-${index}`} /></div>))}
+      </div>
+
+      <div className="footer">
+        <h1>foooter</h1>
+      </div>
+    </>
+
+
   )
 }
 
