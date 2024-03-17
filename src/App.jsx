feff887eaabe6ed2fda97b69e6046f925fbcfd6c
robin croft <rob@robincroft.com>
2024-03-17 15:11:05 +0000

    buttons

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,26 @@ function App() {
 
   const desktopThmbnails = import.meta.glob('./assets/1ironMaiden/thumbnails/bak/*.{jpg,jpeg,png,svg}');
   const mobileThumbnails = import.meta.glob('./assets/1ironMaiden/thumbnails/*.{jpg,jpeg,png,svg}');
-  
-  const [type, setType] = useState('desktop');
+  const buttonItems = import.meta.glob('./buttons/*.{jpg,jpeg,png,svg}');
 
+  const [type, setType] = useState('desktop');
   const [images, setImages] = useState([]);
+  const [buttons, setButtons] = useState([]);
 
   useEffect(() => {
     console.log("use effect ");
     onSetDescktop();
+
+    const items = [];
+    for (const button in buttonItems) {
+      // Extract the URL using a new URL object (prevents path issues)
+      items.push(new URL(button, import.meta.url).href);
+      // items.push(new URL(image, import.meta.url).href);
+    }
+
+    console.log("check ", items);
+    setButtons(items);
+
   }, []);
 
   const onSetDescktop = () => {
@@ -59,10 +71,12 @@ function App() {
           <a href="http://horseflaps.com" target="_blank">
             <img src="src/assets/homeImages/banner.jpg" alt="horseflaps.com banner" /></a>
         </div>
-        <button className='button' onClick={onChangeType}>{type}</button>
+        <div className="buttons__wrapper">          
+        {buttons.map((image, index) => (<div key={`image-${index}`}><img src={image} alt={`image-${index}`} /></div>))}
+      </div>
       </div>
 
-
+      
       <div className="thumbnails">
         {images.map((image, index) => (<div key={`image-${index}`}><img style={{ height: "100%", width: "100%" }} src={image} alt={`image-${index}`} /></div>))}
       </div>
